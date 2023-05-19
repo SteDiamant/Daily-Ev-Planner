11559d1c57715cbb8982abe59e0fcc39f2c94fe5
SteDiamant <123244340+SteDiamant@users.noreply.github.com>
2023-05-19 13:09:33 +0200

    Update PVGIS.py

--- pages/PVGIS.py
+++ pages/PVGIS.py
@@ -7,6 +7,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import datetime as dt
 # Define conversion function
+
 def convert_timestamp(timestamp_str):
     timestamp = pd.Timestamp(timestamp_str)
     new_timestamp = timestamp.tz_convert('Europe/Amsterdam').strftime('%m/%d/%Y %H:%M')
@@ -47,14 +48,40 @@ new_data['TimestamptMonth'] = new_data['Timestampt'].dt.month
 new_data['TimestamptDay'] = new_data['Timestampt'].dt.day
 new_data['TimestamptHour'] = new_data['Timestampt'].dt.hour
 new_data['TimestamptMinute'] = new_data['Timestampt'].dt.minute
+
 df['Timestampt'] = df.index
 df['TimestamptMonth'] = df['Timestampt'].dt.month
 df['TimestamptDay'] = df['Timestampt'].dt.day
 df['TimestamptHour'] = df['Timestampt'].dt.hour
 df['TimestamptMinute'] = df['Timestampt'].dt.minute
 df = df.dropna(subset=['PV (W)'])
-st.dataframe(df)
-st.dataframe(new_data)
+# st.dataframe(df)
+# st.dataframe(new_data)
+st.markdown('## _Prediction of Solar Production_')
+st.markdown('### _The prediction is based made for the duration (27-4-23 and 3-5-23)_')
+st.markdown("""
+                <li>Data is collected from PVGIS and Solcast for the duration (27-4-23 and 3-5-23)
+                <li>** TimestamptMinute **: This column represents the minute component of the timestamp. It indicates the specific minute of the hour for a given data point.
+
+                <li>TimestamptHour: This column represents the hour component of the timestamp. It indicates the specific hour of the day for a given data point.
+
+                <li>TimestamptMonth: This column represents the month component of the timestamp. It indicates the specific month of the year for a given data point.
+
+                <li>TimestamptDay: This column represents the day component of the timestamp. It indicates the specific day of the month for a given data point.
+
+                <li>CloudOpacity: This column represents the cloud opacity. Cloud opacity refers to the extent to which clouds obstruct sunlight. It indicates the amount of cloud cover at a particular time.
+
+                <li>Dhi: This column represents the diffuse horizontal irradiance. DHI refers to the amount of solar radiation received from the sky dome scattered by the atmosphere. It represents the solar radiation received from all directions except the direct sun.
+
+                <li>Dni: This column represents the direct normal irradiance. DNI refers to the amount of solar radiation received per unit area from the direction of the sun on a surface that is perpendicular to the sun's rays. It represents the direct solar radiation.
+
+                <li>Ebh: This column represents the estimated actual horizontal irradiance. EBH refers to the amount of solar radiation received on a horizontal surface.
+
+                <li>Ghi: This column represents the global horizontal irradiance. GHI refers to the total amount of solar radiation received on a horizontal surface, including both direct and diffuse solar radiation.
+
+                
+                
+                """,unsafe_allow_html=True)
 # # Split data into training and validation sets
 
 X_train = df[['TimestamptMinute','TimestamptHour','TimestamptMonth','TimestamptDay','CloudOpacity', 'Dhi', 'Dni', 'Ebh', 'Ghi']]
@@ -96,7 +123,8 @@ df_filtered = df.loc['2021-04-26 11:00:00':'2021-05-03 11:00:00']
 # create a new figure and axes
 fig, ax = plt.subplots()
 dt_range = pd.date_range(start='2021-04-26 11:00:00', end='2021-05-03 11:00:00', freq='30T')
-x = np.arange(len(dt_range))
+x = pd.date_range(start='2023-04-26 11:00:00', end='2023-05-03 11:00:00', freq='30T')
+plt.xticks(rotation=25)
 # plot the predicted PV line on the axes
 ax.plot(x, results['PV_prediction (W)'], color='red', label='Predicted PV')
 
